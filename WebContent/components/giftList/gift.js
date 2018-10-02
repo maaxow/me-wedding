003f7ac457df937eccb615239e54ff82fe6d152c
rosemx <maxime.rose@cgi.com>
2018-10-02 13:18:13 +0200

    gift list

--- WebContent/components/giftList/gift.js
+++ WebContent/components/giftList/gift.js
@@ -2,7 +2,13 @@ angular.module('app.controllers.gift', [])
 
 .controller('GiftController', function($scope, $http, $interval, CONSTANTS){
 	$scope.gifts = [];
-
+	$scope.totalAmount = function(){
+		var amount = 0;
+		for(var index in $scope.gifts){
+			amount += $scope.gifts[index].total;
+		}
+		return amount;
+	}
 	$http.get("components/giftList/gifts.json").then(function(gifts){
 		$scope.gifts = gifts.data;
 	});
