ab871f38a33a5e79174049c4f044bbebe09a0319
rosemx <maxime.rose@cgi.com>
2018-10-02 14:03:54 +0200

    stuff

--- WebContent/components/giftList/gift.js
+++ WebContent/components/giftList/gift.js
@@ -1,21 +1,39 @@
 angular.module('app.controllers.gift', [])
 
-.controller('GiftController', function($scope, $http, $interval, CONSTANTS){
-	$scope.gifts = [];
-	$scope.totalAmount = function(){
-		var amount = 0;
-		for(var index in $scope.gifts){
-			amount += $scope.gifts[index].total;
+.controller('GiftController', ["$scope", "$http", "$timeout", "CONSTANTS", 
+	function($scope, $http, $timeout, CONSTANTS){
+	
+		$scope.gifts = [];
+		$scope.currentAmount = 2000;
+		$scope.progressBarStyle = {
+				width: '50%'
+		};
+		$scope.totalAmount = 0;
+		var _initialize = function(){
+			$scope.calculTotalAmount();
+			$scope.calculWidthProgress();
+		};
+		$scope.calculTotalAmount = function(){
+			var amount = 0;
+			for(var index in $scope.gifts){
+				amount += $scope.gifts[index].total;
+			}
+			$scope.totalAmount = amount;
+			return amount;
 		}
-		return amount;
-	}
-	$http.get("components/giftList/gifts.json").then(function(gifts){
-		$scope.gifts = gifts.data;
-	});
-
-	$scope.calculWidthProgress = function(gift){
-		var percent = (gift.amount*100) / gift.total;
-		return percent * 518.6 / 100;
-	};
-
-});
+		$http.get("components/giftList/gifts.json").then(function(gifts){
+			$scope.gifts = gifts.data;
+		});
+	
+		$scope.calculWidthProgress = function(){
+			var percent = ($scope.currentAmount*100) / 	$scope.totalAmount;
+			$scope.progressBarStyle.width = percent + '%';
+			return percent;
+		};
+		
+		//Initialization
+		$timeout(function(){
+			_initialize();
+		},10);
+		$scope.calculWidthProgress();
+}]);
